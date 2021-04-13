742f86a27282ef85b1f5b00caddb0aa8d5060002
Guillaume Geoffroy <guillaume.geoffroy@epitech.eu>
2021-04-13 20:29:15 +0200

    feat: add cors plicy

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
 
 var indexRouter = require('./routes/index');
 
 var app = express();
 
+app.use(cors());
+const opts = {
+  "origin": "*",
+  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+};
+app.options('*', cors(opts));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
